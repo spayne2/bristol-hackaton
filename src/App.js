602e0ceb57befeb51348059998249af0a57e06e5
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2023-06-20 12:11:26 +0100

    navbar changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 import Student from './student';
 import Employee from './employee';
+import SFeedback from './SFeedback';
+import EFeedback from './EFeedback';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Student />} />
           <Route path="/employees" element={<Employee />} />
+          <Route path="/SFeedback" element={<SFeedback />} />
+          <Route path="/EFeedback" element={<EFeedback />} />
         </Routes>
       </div>
     </Router>
